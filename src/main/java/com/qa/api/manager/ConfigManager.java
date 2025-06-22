04c8e83fce9e02cf1f1ce5eee7e26c68d04f3f06
sonugadwe <sonugadwe06@gmail.com>
2025-06-22 14:10:42 +0530

    updated sanity

--- src/main/java/com/qa/api/manager/ConfigManager.java
+++ src/main/java/com/qa/api/manager/ConfigManager.java
@@ -8,8 +8,19 @@ public class ConfigManager {
 
 	private static Properties properties = new Properties();
 	
+	// mvn clean install -Denv=qa/stage/dev/uat/prod
+			// mvn clean install -Denv=qa
+			// mvn clean install -- if env is not given, then run test cases on QA env by default.
+			// env -- environment variable(system)
+	
 	static {//it loads initially before loading config manager and it going to CMA memory
-		InputStream input = ConfigManager.class.getClassLoader().getResourceAsStream("config/config.properties");
+		
+		String envName = System.getProperty("env", "prod");
+		System.out.println("running tests on env: " + envName);
+		String fileName = "config_" + envName + ".properties"; // config_qa.properties
+
+		InputStream input = ConfigManager.class.getClassLoader().getResourceAsStream(fileName);
+		//InputStream input = ConfigManager.class.getClassLoader().getResourceAsStream("config/config.properties");
 		if(input != null) {
 			try {
 				properties.load(input);
--- src/test/java/com/qa/api/base/BaseTest.java
+++ src/test/java/com/qa/api/base/BaseTest.java
@@ -4,6 +4,7 @@ import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.BeforeTest;
 
 import com.qa.api.client.RestClient;
+import com.qa.api.manager.ConfigManager;
 
 import io.qameta.allure.restassured.AllureRestAssured;
 import io.restassured.RestAssured;
@@ -13,32 +14,40 @@ public class BaseTest {
 
 	protected RestClient restClient;
 
-	//********** API Base URLs*******//
-		protected final static String BASE_URL_GOREST = "https://gorest.co.in";
-		protected final static String BASE_URL_CONTACTS = "https://thinking-tester-contact-list.herokuapp.com";
-		protected final static String BASE_URL_REQRES = "https://reqres.in";
-		protected final static String BASE_URL_BASIC_AUTH = "https://the-internet.herokuapp.com";
-		protected final static String BASE_URL_PRODUCTS = "https://fakestoreapi.com";
-		protected final static String BASE_URL_OAUTH2_AMADEUS = "https://test.api.amadeus.com";
-		protected final static String BASE_URL_ERGAST_CIRCUIT = "http://ergast.com";
+	// ********** API Base URLs*******//
 
-		
-		//********** API EndPoints*******//
-		protected final static String GOREST_USERS_ENDPOINT = "/public/v2/users";
-		protected final static String CONTACTS_LOGIN_ENDPOINT = "/users/login";
-		protected final static String CONTACTS_ENDPOINT = "/contacts";
-		protected final static String REQRES_ENDPOINT = "/api/users";
-		protected final static String BASIC_AUTH_ENDPOINT = "/basic_auth";
-		protected final static String PRODUCTS_ENDPOINT = "/products";
-		protected final static String AMADEUS_OATUH2_TOKEN_ENDPOINT = "/v1/security/oauth2/token";
-		protected final static String AMADEUS_FLIGHT_DEST_ENDPOINT = "/v1/shopping/flight-destinations";
-		protected final static String ERGAST_CIRCUIT_ENDPOINT = "/api/f1/2017/circuits.xml";
-		
-		
-		@BeforeSuite
-		public void setupAllureReport() {
-			RestAssured.filters(new AllureRestAssured());
-		}
+	protected static String BASE_URL_GOREST;
+	protected static String BASE_URL_REQRES;
+
+	// protected final static String BASE_URL_GOREST = "https://gorest.co.in";
+	protected final static String BASE_URL_CONTACTS = "https://thinking-tester-contact-list.herokuapp.com";
+	protected final static String BASE_URL_BASIC_AUTH = "https://the-internet.herokuapp.com";
+	protected final static String BASE_URL_PRODUCTS = "https://fakestoreapi.com";
+	protected final static String BASE_URL_OAUTH2_AMADEUS = "https://test.api.amadeus.com";
+	protected final static String BASE_URL_ERGAST_CIRCUIT = "http://ergast.com";
+
+	// ********** API EndPoints*******//
+	protected final static String GOREST_USERS_ENDPOINT = "/public/v2/users";
+	protected final static String CONTACTS_LOGIN_ENDPOINT = "/users/login";
+	protected final static String CONTACTS_ENDPOINT = "/contacts";
+	protected final static String REQRES_ENDPOINT = "/api/users";
+	protected final static String BASIC_AUTH_ENDPOINT = "/basic_auth";
+	protected final static String PRODUCTS_ENDPOINT = "/products";
+	protected final static String AMADEUS_OATUH2_TOKEN_ENDPOINT = "/v1/security/oauth2/token";
+	protected final static String AMADEUS_FLIGHT_DEST_ENDPOINT = "/v1/shopping/flight-destinations";
+	protected final static String ERGAST_CIRCUIT_ENDPOINT = "/api/f1/2017/circuits.xml";
+
+//	@BeforeSuite
+//	public void setupAllureReport() {
+//		RestAssured.filters(new AllureRestAssured());
+//	}
+	
+	@BeforeSuite
+	public void initSetup() {
+		RestAssured.filters(new AllureRestAssured());
+		BASE_URL_GOREST = ConfigManager.get("baseurl.gorest").trim();
+		BASE_URL_REQRES = ConfigManager.get("baseurl.reqres").trim();
+	}
 
 	@BeforeTest
 	public void setup() {
